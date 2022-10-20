80c99dc11c67212dfcd6a48038a8a24e856602b7
Tatiana Rudikova <tatjana_tv@list.ru>
2022-10-20 16:17:50 +0300

    changed the BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,9 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int bonus;
 
-        if (cost > 1000) {
-            bonus = cost / 100;
-        } else {
-            bonus = 0;
-        }
-        return bonus;
+        int oneMileForRubles = 20;
+        int miles = cost / oneMileForRubles;
+
+        return miles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 1100;
+        int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
     }
